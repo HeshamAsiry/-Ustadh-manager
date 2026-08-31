1cd2c2ecf2bc62131756aebf728d9a198b98afc3
HeshamAsiry <66654236+HeshamAsiry@users.noreply.github.com>
2026-09-01 00:27:56 +0300

    Use redesigned personal schedule

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,3 +1,6 @@
 import AppShell from "../../components/app-shell";
-import { PersonalSchedule } from "../../components/admin-modules";
-export default function Page(){return <AppShell><PersonalSchedule/></AppShell>}
+import PersonalScheduleV2 from "../../components/personal-schedule-v2";
+
+export default function Page(){
+  return <AppShell><PersonalScheduleV2/></AppShell>
+}
