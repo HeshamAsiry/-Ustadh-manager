ab2ad0a2691c1dde98d6f69e2a2a0621ee0b6508
HeshamAsiry <66654236+HeshamAsiry@users.noreply.github.com>
2026-09-22 00:03:55 +0300

    refactor: build integrated alerts center

--- app/alerts/page.tsx
+++ app/alerts/page.tsx
@@ -1,2 +1,2 @@
-import ManagementModule from "../../components/management-module";
-export default function Page(){return <ManagementModule kind="alerts"/>}
+import AlertsPage from "./alerts-page";
+export default function Page(){return <AlertsPage/>}
